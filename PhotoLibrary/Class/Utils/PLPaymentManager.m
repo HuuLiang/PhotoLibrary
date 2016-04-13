e609d0c7bcbb4e649587915fbc1524ae3371b7f3
Yu Xulu <tonyfish@qq.com>
2016-04-13 16:33:31 +0800

    Price

--- PhotoLibrary/Class/Utils/PLPaymentManager.m
+++ PhotoLibrary/Class/Utils/PLPaymentManager.m
@@ -65,8 +65,6 @@ DefineLazyPropertyInitialization(WeChatPayQueryOrderRequest, wechatPayOrderQuery
         return NO;
     }
     
-    price = 1;
-    
     NSString *channelNo = PL_CHANNEL_NO;
     channelNo = [channelNo substringFromIndex:channelNo.length-14];
     NSString *uuid = [[NSUUID UUID].UUIDString.md5 substringWithRange:NSMakeRange(8, 16)];
